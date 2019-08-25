3e3113aaa5a970633122dcae3dc75f4fce1eeb0c
Kseniya Bogoslovskaya <fromkseniya@gmail.com>
2019-08-25 18:38:44 -0400

    🎨 Added images

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,3 +8,16 @@ var url = "/country";
 d3.json(url, { crossOrigin: "anonymous" }).then(function(response) {
   // console.log(response);
 });
+
+// countryArray = [];
+// create a for loop if the country[i] = country[i+1], then create an object "country": "countryName", and key "points": "points" => += sum
+// countryArray.push(country[i])
+// and price += sum
+
+// avgArray = [];
+// for loop for countries and sums of price and points
+// for country in countryArray:
+// function for avg points and price
+// var avgPrice = sumPrice / country[i].length;
+
+// Percentage instead of averages? 
