5589ed10b609f878168405987444b3963b1e4cfc
Kseniya Bogoslovskaya <fromkseniya@gmail.com>
2019-08-31 14:37:34 -0400

    ✨ Leaflet markers with bindPopup

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,34 +5,54 @@ var myMap = L.map("map", {
 });
 
 // Adding tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-}).addTo(myMap);
+L.tileLayer(
+  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  }
+).addTo(myMap);
 
-// var geojsonLayer = new L.GeoJSON.AJAX("geojson.json");
-
-// var geojsonLayer = L.geoJson.ajax("static/js/wine.json",{
-//   middleware:function(data){
-//       return esri2geoOrSomething(json);
-//   }
-// });
-
-
+var geojson;
 
 // Grab the data with d3
-
-// var geojson;
-
 d3.json("static/js/wine.json").then(function(data) {
-  L.geoJson(data).addTo(myMap);
+  // L.geoJson(data).addTo(myMap);
   // console.log(data);
-
-
   // Create a new choropleth layer
-  // geojson = L.choropleth(data, {   
-  // })
-});
+  geojson = L.choropleth(data, {
+    // Define what  property in the features to use
+    valueProperty: "points",
 
+    // Set color scale
+    scale: ["#ffffb2", "#b10026"],
+
+    // Number of breaks in step range
+    steps: 10,
+
+    // q for quartile, e for equidistant, k for k-means
+    mode: "q",
+    style: {
+      // Border color
+      color: "#fff",
+      weight: 1,
+      fillOpacity: 0.8
+    },
+
+    // Binding a pop-up to each layer
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(
+        feature.properties.country +
+          ", " +
+          feature.properties.points+
+          "<br>Median Price per bottle of wine:<br>" +
+          "$" +
+          feature.properties.price
+      );
+    }
+  }).addTo(myMap);
+
+});
