8606039b93d7851aef279e5c721e0b7e6b49e0dd
Kseniya Bogoslovskaya <fromkseniya@gmail.com>
2019-09-11 08:22:32 -0400

    🎨 Top 5countries placed on the map, reformatted

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,21 +1,17 @@
+// leaflet providers link to pick the tileLayer => https://leaflet-extras.github.io/leaflet-providers/preview/
+
 // Creating map object
 var myMap = L.map("map", {
   center: [52.7954791, -0.5402403],
   zoom: 4
 });
 
-// Adding tile layer
 L.tileLayer(
-  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
-  {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: "mapbox.light",
-    accessToken: API_KEY
-  }
-).addTo(myMap);
-
+  'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
+  accessToken: API_KEY
+}).addTo(myMap);
 
 var geojson;
 
@@ -23,25 +19,19 @@ var geojson;
 d3.json("static/js/wine_polygon.json").then(function(data) {
 
   L.geoJson(data).addTo(myMap);
-  // console.log(data);
+
   // Create a new choropleth layer
   geojson = L.choropleth(data, {
     // Define what  property in the features to use
     valueProperty: "polygon",
 
-    // Set color scale
-    scale: ["#ffffb2", "#b10026"],
-
-    // Number of breaks in step range
-    steps: 10,
-
     // q for quartile, e for equidistant, k for k-means
     mode: "q",
     style: {
       // Border color
-      color: "#202040",
-      weight: 1,
-      fillOpacity: 0.8
+      color: "#540e33",
+      weight: 2,
+      fillOpacity: 0.7
     },
 
     // Binding a pop-up to each layer
